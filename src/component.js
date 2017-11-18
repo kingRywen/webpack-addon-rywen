0fb1dfe74053a2d4a216829e8336967dd1fe52a8
kingRywen <673895045@qq.com>
2017-11-18 19:59:26 +0800

    懒加载

--- src/component.js
+++ src/component.js
@@ -1,10 +1,12 @@
-import printMe from './print.js'
 export default function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
 
   btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+  btn.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default
+    print()
+  })
 
   element.appendChild(btn);
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
-// import Promise from 'promise-polyfill'
 import './style/main.scss'
 import component from './component'
-import printMe from './print.js'
-import { square, cube } from "./math"
-// import _ from 'lodash'
+// import printMe from './print.js'
+import _ from 'lodash'
 
 var Promise = require('es6-promise').Promise
 
-console.log(square(2))
-
-printMe()
-
-async function getComponent() {
-  var element = document.createElement('div')
-  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash')
-  element.innerHTML = _.join(['hello', 'world'], ' ')
-  return element
-}
-
-getComponent().then(component => {
-  document.body.appendChild(component)
-})
-
-if (module.hot) {
-  module.hot.accept();
-}
-
+document.body.appendChild(component())
